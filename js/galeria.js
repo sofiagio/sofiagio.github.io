b12ea57ff927e8f01c42a88054320cc295192557
Sofia Gio <165825686+sofiagio@users.noreply.github.com>
2024-04-02 19:26:33 -0300

    incluindo js

--- js/galeria.js
+++ js/galeria.js
@@ -1,36 +1,17 @@
 
-document.addEventListener("DOMContentLoaded", function() {
-    // Cria um objeto XMLHttpRequest
-    var xhr = new XMLHttpRequest();
-    // Abre uma conexão para a pasta "images/" no servidor
-    xhr.open("GET", "../img/galeria/", true);
-    // Define a função a ser executada quando a solicitação Ajax for concluída
-    xhr.onreadystatechange = function() {
-        // Verifica se a solicitação foi bem-sucedida e se o estado está completo
-        if (this.readyState === 4 && this.status === 200) {
-            // Obtém a resposta da solicitação como uma string de texto
-            var images = this.responseText;
-            // Cria um novo objeto DOMParser para analisar a string de texto como um documento HTML
-            var parser = new DOMParser();
-            var htmlDoc = parser.parseFromString(images, "text/html");
-            // Obtém todas as tags "a" do documento HTML
-            var links = htmlDoc.getElementsByTagName("a");
-            // Itera por cada tag "a"
-            for (var i = 0; i < links.length; i++) {
-                // Obtém o nome do arquivo do link
-                var file = links[i].getAttribute("href");
-                // Verifica se o arquivo é uma imagem
-                if (file.match(/\.(jpe?g|png|gif)$/)) {
-                // Cria uma nova imagem
-                var img = document.createElement("img");
-                // Define o atributo "src" da imagem com o caminho completo para o arquivo de imagem na pasta "images/"
-                img.src = "images/" + file;
-                // Adiciona a nova imagem à div "image-gallery"
-                document.getElementById("image-gallery").appendChild(img);
-                }
-            }
-        }
-    };
-// Envia a solicitação Ajax
-xhr.send();
+document.addEventListener('DOMContentLoaded', function() {
+    const galeria = document.querySelector('.galeria');
+    const urlJson = 'https://sofiagio.github.io/json/galeria.json'; // Substitua pela URL correta do seu arquivo JSON
+  
+    fetch(urlJson)
+      .then(response => response.json())
+      .then(data => {
+        data.imagens.forEach(imagem => {
+          const img = document.createElement('img');
+          img.src = `https://sofiagio.github.io/img/galeria/${imagem}`; // Substitua pela URL correta
+          img.alt = imagem;
+          galeria.appendChild(img);
+        });
+      })
+      .catch(error => console.error('Erro ao carregar imagens:', error));
 });
