3da8744aedd68ef7907f9c2622bc60d105229db2
Sofia Gio <165825686+sofiagio@users.noreply.github.com>
2024-04-02 19:34:30 -0300

    correção de links

--- js/galeria.js
+++ js/galeria.js
@@ -1,14 +1,14 @@
 
 document.addEventListener('DOMContentLoaded', function() {
     const galeria = document.querySelector('.galeria');
-    const urlJson = 'https://github.com/sofiagio/sofiagio.github.io/tree/github.com/sofiagio/sofiagio.github.io/json/galeria.json'; // Substitua pela URL correta do seu arquivo JSON
+    const urlJson = 'https://raw.githubusercontent.com/sofiagio/sofiagio.github.io/github.com/sofiagio/sofiagio.github.io/json/galeria.json'; // Substitua pela URL correta do seu arquivo JSON
   
     fetch(urlJson)
       .then(response => response.json())
       .then(data => {
         data.imagens.forEach(imagem => {
           const img = document.createElement('img');
-          img.src = `https://github.com/sofiagio/sofiagio.github.io/tree/github.com/sofiagio/sofiagio.github.io/img/galeria/${imagem}`; // Substitua pela URL correta
+          img.src = `https://raw.githubusercontent.com/sofiagio/sofiagio.github.io/github.com/sofiagio/sofiagio.github.io/img/galeria/${imagem}`; // Substitua pela URL correta
           img.alt = imagem;
           galeria.appendChild(img);
         });
