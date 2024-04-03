98f8052ffac9d7e7124b9e4cc65f28ece59cf903
Sofia Gio <165825686+sofiagio@users.noreply.github.com>
2024-04-02 21:29:55 -0300

    teste print ruby

--- _plugins/generate_images_json.rb
+++ _plugins/generate_images_json.rb
@@ -6,7 +6,9 @@ Jekyll::Hooks.register :site, :post_write do |site|
   
     # Lista de imagens
     images = Dir.children(images_dir).select { |f| File.file?(File.join(images_dir, f)) }
-  
+
     # Gera o JSON
     File.write(File.join(site.dest, 'json/galeria.json'), { imagens: images }.to_json)
+
+    puts site.dest
   end
