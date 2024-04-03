2d77f3ef660523a1947fb8fd3773e0323a911cb5
Sofia Gio <165825686+sofiagio@users.noreply.github.com>
2024-04-02 21:03:10 -0300

    teste caminho plugin

--- _plugins/generate_images_json.rb
+++ _plugins/generate_images_json.rb
@@ -2,7 +2,7 @@ Jekyll::Hooks.register :site, :post_write do |site|
     require 'json'
   
     # Caminho para a pasta com as imagens
-    images_dir = File.join(site.source, '../img/galeria')
+    images_dir = File.join(site.source, 'img/galeria/')
   
     # Lista de imagens
     images = Dir.children(images_dir).select { |f| File.file?(File.join(images_dir, f)) }
